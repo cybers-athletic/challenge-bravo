c5de6468255f966d000a2b0baaff479b0305ac6b
Celso Inácio <ceeeelso@gmail.com>
2018-10-31 16:39:23 -0300

    Revert "Desafio Concluido!"

--- sl4ureano/desafio1/index.py
+++
@@ -1,6 +0,0 @@
-lista = ['-2', '07', '0', '-5', '8', '4']
-numero_inteiro = raw_input("Digite um Numero inteiro: ")
-lista.append(numero_inteiro)
-maior = (max(int(number) for number in lista))
-menor = (min(int(number) for number in lista))
-print("Maior Numero: %s \nMenor Numero: %s" % (maior,menor))
